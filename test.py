b1e8d7ac3b79166226d57c7577b2e4a5dbcb75cf
Hyunkel <dhyunkel@gmail.com>
2017-12-05 14:56:03 +0700

    update

--- test.py
+++ test.py
@@ -1,18 +1,15 @@
 import demo
 import unittest
 import sys
-sys.stdout.flush()
 
 
 class UnittestSuper(unittest.TestCase, demo.DemoTestPython):
 
     def setUp(self):
         print("Thiep Lap Cac Truong Hop Cua Unittest")
-        sys.stdout.flush()
 
     def tearDown(self):
         print("Don Dep Cac TH cua Unittest")
-        sys.stdout.flush()
 
 
 class TestNow(UnittestSuper):
